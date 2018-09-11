eee06314dcfc7f4ac31b482a1cb0ea9c9fdefbf3
Alexander Amling <aja7073@rit.edu>
2018-09-10 21:48:51 -0400

    Basic Project setup Created nessicary objects: QueueArray and QueueItem Set them up for generic data types Wrote Get indexer

---
+++ QueueArray/QueueArray/QueueArray.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueArray
+{
+    class QueueArray<T>
+    {
+        private Queue<QueueItem<T>> queue;
+        private int length;
+
+        public int Length { get { return length; } }
+
+        public T this[int index]
+        {
+            get
+            {
+                QueueItem<T> temp; // var to hold items while looping through the queue
+
+                // get the desired index to the front of the queue
+                for (int x = 0; x < index; x++)
+                {
+                    temp = queue.Dequeue();
+                    queue.Enqueue(temp);
+                }
+
+                T data = queue.Peek().Data; // retrieve desired data
+
+                // rotate back through to the initia; index
+                for (int x = 0; x < length - index; x++)
+                {
+                    temp = queue.Dequeue();
+                    queue.Enqueue(temp);
+                }
+
+                return data;
+            }
+            set { }
+        }
+
+        public QueueArray(int len)
+        {
+            queue = new Queue<QueueItem<T>>();
+            length = len;
+            
+            // need to populate the queue with null values, as if it was an array
+            for (int x = 0; x < len; x++)
+            {
+                queue.Enqueue(new QueueItem<T>());
+            }
+        }
+    }
+}
---
+++ QueueArray/QueueArray/QueueItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueArray
+{
+    // This entire class is needed simply to create placeholder objects in the queue until more data is added
+    class QueueItem<T>
+    {
+        public T Data { get; set; }
+    }
+}
